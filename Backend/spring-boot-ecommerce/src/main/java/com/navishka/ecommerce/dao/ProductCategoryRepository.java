49f2e7039df04ee06ead4db7251242b77c3c8d07
Navishka Kularathna <navishka.randika@gmail.com>
2020-06-13 01:13:33 +0530

    Repositories created

---
+++ Backend/spring-boot-ecommerce/src/main/java/com/navishka/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.navishka.ecommerce.dao;
+
+import com.navishka.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productionCategory", path = "production-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
