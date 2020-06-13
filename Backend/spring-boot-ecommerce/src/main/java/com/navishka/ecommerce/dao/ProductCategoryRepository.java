59f6ce45b607864c49dd30485445e9a98aad6188
Navishka Kularathna <navishka.randika@gmail.com>
2020-06-13 14:44:46 +0530

    REST APIs - configured for read only

--- Backend/spring-boot-ecommerce/src/main/java/com/navishka/ecommerce/dao/ProductCategoryRepository.java
+++ Backend/spring-boot-ecommerce/src/main/java/com/navishka/ecommerce/dao/ProductCategoryRepository.java
@@ -4,6 +4,6 @@ import com.navishka.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "productionCategory", path = "production-category")
+@RepositoryRestResource(collectionResourceRel = "productionCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
