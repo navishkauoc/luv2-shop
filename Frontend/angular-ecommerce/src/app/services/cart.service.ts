5061dab264d6c611154e37b4ebbba7cfeee883c1
navishkauoc <navishka.randika@gmail.com>
2021-03-15 23:04:35 +0530

    Computing totals - cart service

--- Frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ Frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -46,7 +46,34 @@ export class CartService {
   }
   
   computeCartTotals() {
-    throw new Error('Method not implemented.');
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let currentCartItem of this.cartItems) {
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+    }
+
+    // Publish the new values - all the subscribers will receive the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    // Log cart data, just for debugging purposes
+    this.logCartData(totalPriceValue, totalQuantityValue);
+
+  }
+
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    console.log('Contents of the cart: ');
+
+    for (let tempCartItem of this.cartItems) {
+      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
+      console.log(`Name: ${tempCartItem.name}, quantity: ${tempCartItem.quantity}, unit price: ${tempCartItem.unitPrice}, sub total price: ${subTotalPrice}`);
+    }
+
+    console.log(`Total price: ${totalPriceValue.toFixed(2)}, total quantity: ${totalQuantityValue}`);
+    console.log('-----');
+
   }
 
 }
