b55b1bb43bda0ce9c16851ab1baed487344edd19
navishkauoc <navishka.randika@gmail.com>
2021-03-21 12:27:47 +0530

    CartDetailsComponent: retriving table data

--- Frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ Frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailsComponent implements OnInit {
 
-  constructor() { }
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    // Get a handle to the cart items
+    this.cartItems = this.cartService.cartItems;
+
+    // Subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // Subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+    // Compute cart total price and total quantity
+    this.cartService.computeCartTotals();
   }
 
 }
