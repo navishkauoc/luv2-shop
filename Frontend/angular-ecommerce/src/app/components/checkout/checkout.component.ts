9d0ccc82616452081a7b5e33813ff7e0da0d3aa1
navishkauoc <navishka.randika@gmail.com>
2021-04-17 20:51:42 +0530

    Validation rules added to shipping address form

--- Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -119,6 +119,12 @@ export class CheckoutComponent implements OnInit {
   get lastName() { return this.checkoutFormGroup.get('customer.lastName');}
   get email() { return this.checkoutFormGroup.get('customer.email');}
 
+  get shippingAddressStreet() { return this.checkoutFormGroup.get('shippingAddress.street');}
+  get shippingAddressCity() { return this.checkoutFormGroup.get('shippingAddress.city');}
+  get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state');}
+  get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country');}
+  get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode');}
+
   handleMonthsAndYears() {
     const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
 
