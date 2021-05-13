438e966d211a540f4ba64f215e2043c7e5c544a3
navishkauoc <navishka.randika@gmail.com>
2021-05-13 23:08:01 +0530

    Login component created

--- Frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ Frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,6 +1,8 @@
 export default {
-    oidc: '0oaq9t5xbn3WavnQq5d6',
-    issuer: 'https://dev-55000810.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
-    scopes: ['openid', 'profile', 'email']
+    oidc: {
+        clientId: '0oaq9t5xbn3WavnQq5d6',
+        issuer: 'https://dev-55000810.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
 }
