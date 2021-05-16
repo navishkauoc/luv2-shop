debd0b4d943231139fe3442e77e3a20e7a4b9641
navishkauoc <navishka.randika@gmail.com>
2021-05-16 19:16:36 +0530

    Backend secured with Okta integration

---
+++ Backend/spring-boot-ecommerce/src/main/java/com/navishka/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.navishka.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // Protect endpoint /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        // Add CORS filtes
+        http.cors();
+
+        // Force a non-empty response body for 401's
+        Okta.configureResourceServer401ResponseBody(http);
+    }
+}
