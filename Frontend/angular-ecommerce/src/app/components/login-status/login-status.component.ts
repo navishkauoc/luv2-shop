27ffbe0e4101ef669484c1b2b23c887a7d2be24e
navishkauoc <navishka.randika@gmail.com>
2021-05-16 16:43:13 +0530

    View order history - front-end

--- Frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ Frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -11,6 +11,8 @@ export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
   userFullName: string;
 
+  storage: Storage = sessionStorage;
+
   constructor(private oktaAuthService: OktaAuthService) { }
 
   ngOnInit(): void {
@@ -30,6 +32,10 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuthService.getUser().then(
         res => {
           this.userFullName = res.name;
+
+          // Retrieve the user's email from authenticated response
+          const theEmail = res.email;
+          this.storage.setItem('userEmail', JSON.stringify(theEmail));
         }
       )
     }
